4985224f0144c92383edf0f7b1a87c7733619898
Cap1vas <gat3105@gmail.com>
2025-03-21 20:57:00 -0300

    Modificado

--- helloWorld.java
+++ helloWorld.java
@@ -1,5 +1,7 @@
 public class helloWorld {
     public static void main(String[] args) {
       System.out.println("Hello, World!");
+      System.out.println("Meu nome é " + args[0] + args[1]);
+
   }
 }   
