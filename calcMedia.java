1385514251d4f15fc9c4ac549fe8d049b4df76f7
Cap1vas <gat3105@gmail.com>
2025-03-28 19:08:47 -0300

    Código de média

---
+++ calcMedia.java
@@ -0,0 +1,8 @@
+public class calcMedia {
+    public static void main(String [] args){
+        int valor1 = Integer.parseInt(args[0]);
+        int valor2 = Integer.parseInt(args[1]);
+        int media = (valor1 + valor2)/2;
+        System.out.println("O valor é " +  media);
+    }
+}
